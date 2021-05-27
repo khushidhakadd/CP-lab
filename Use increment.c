1411d08b984d9de5edc939dfd33e5c4c7a000837
khushidhakadd <84145722+khushidhakadd@users.noreply.github.com>
2021-05-27 23:12:29 +0530

    Create Use increment.c

---
+++ Use increment.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a=9;
+    int b=6;
+    a=a++;
+    printf("The value of a is %d",a);
+    a=++a;
+    printf("\nThe value of a is %d",a);
+    b=b--;
+    printf("\nThe value of b is %d",b);
+    b=--b;
+    printf("\nThe value of b is %d",b);
+    
+    return 0;
+    
+}
+
+
