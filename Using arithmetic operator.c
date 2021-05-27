54139cf0c12d6251f592fae5f683966ab122159f
khushidhakadd <84145722+khushidhakadd@users.noreply.github.com>
2021-05-27 23:11:00 +0530

    Create Using arithmetic operator.c

---
+++ Using arithmetic operator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=100;
+    int b=5;
+    //Using arithmatic operator create a basic programe
+    
+    printf("Sum of a+b is %d",a+b);//addition of a and b
+    printf("\nSubtraction of a-b is %d",a-b);//subtraction of a and b//
+    printf("\nMultiply of a*b is %d",a*b);//multiplaction of a and b
+    printf("\nDivision of a/b is %d",a/b);//division of a and b
+    printf("\nReminder of a%%b is %d",a%b);//reminder of  a and b
+    return 0;
+    
+    
+}
