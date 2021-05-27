83e83ae27f4f6d940cda0e3b959778da5a05d105
khushidhakadd <84145722+khushidhakadd@users.noreply.github.com>
2021-05-27 22:32:36 +0530

    Create Use of variable.c

---
+++ Use of variable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+   int var2=20;
+   int var3=90;
+   printf("Value of first variable is %d", var2);
+   printf("Value of second variable is %d", var3);
+   
+   return 0;
+
+}
