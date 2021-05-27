ce44935b905496fdc1dc27dbdaef27d14e69aab7
khushidhakadd <84145722+khushidhakadd@users.noreply.github.com>
2021-05-27 23:21:48 +0530

    Create Relational operator.c

---
+++ Relational operator.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int x=45;
+    int y=55;
+    
+    //using == operator , if condition is true then it will give 1 otherwise 0
+    x==y;
+    printf("Result is %d\n",x==y);
+    //using != operator , if condition is true then it will give 0 otherwise 1
+    x!=y;
+    printf("Result is %d\n",x!=y);
+    //using <= operator , if condition is true then it will give 1 otherwise 0
+    x<=y;
+    printf("Result is %d\n",x<=y);
+    //using >= operator , if condition is true then it will give 1 otherwise 0
+    x>=y;
+    printf("Result is %d\n",x>=y);
+    //using < operator , if condition is true then it will give 1 otherwise 0
+    x<y;
+    printf("Result is %d\n",x<y);
+    //using > operator , if condition is true then it will give 1 otherwise 0
+    x>y;
+    printf("Result is %d\n",x>y);
+    
+    return 0;
+}
