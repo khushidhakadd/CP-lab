0f827ecdfece80eb762ec7df87ba2edc922ac92f
khushidhakadd <84145722+khushidhakadd@users.noreply.github.com>
2021-07-23 10:55:46 +0530

    Create Odd even.c

---
+++ Odd even.c
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+int main()
+{
+       int a;
+    printf("This is designed by Khushi dhakad");
+    printf("\nEnter value of a:");
+    scanf("%d",&a);
+    if(a%2==0) 
+    {
+    printf("A is even number");
+    }
+    else
+    {
+    printf("A is odd number");
+    }
+    return 0;
+}
