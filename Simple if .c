33a1f29ce12b083693892f2aedc5fce80fdf9e48
khushidhakadd <84145722+khushidhakadd@users.noreply.github.com>
2021-07-23 10:34:52 +0530

    Create Simple if .c

---
+++ Simple if .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+       int a,b;
+    printf("This is designed by Nikhil Bhardwaj");
+    printf("\nEnter the value of a and b:");
+    scanf("%d %d",&a,&b);
+    if (a>b)
+    {
+    printf("\nYou are in if and the a is greater then b");
+    }
+    printf("You are out of if");
+    return 0;
+}
